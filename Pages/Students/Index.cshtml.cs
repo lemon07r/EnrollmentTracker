c980c334f8dd38717c02b71543a33fb56623856e
Lamim <lemon07r@gmail.com>
2019-09-12 15:31:39 -0400

    Initial Commit
    Initial features completed, demo working as intended.

---
+++ Pages/Students/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using EnrollmentTracker.Models;
+
+namespace EnrollmentTracker.Pages.Students
+{
+    public class IndexModel : PageModel
+    {
+        private readonly EnrollmentTracker.Models.SchoolContext _context;
+
+        public IndexModel(EnrollmentTracker.Models.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Student> Student { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Student = await _context.Student.ToListAsync();
+        }
+    }
+}
